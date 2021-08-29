279a6ce3474eb87b24b92fe143fd0708221b44a6
Sofia Orlova <sofia.orlova@telekom.com>
2021-08-29 17:21:44 +0300

    /backend_user_auth: custom authentification middleware

--- backend/controlers/userController.js
+++ backend/controlers/userController.js
@@ -31,4 +31,27 @@ const authUser = asyncHandler(async (req, res) => {
     // })
 })
 
-export { authUser }
+
+// @desc       Get user profile
+// @route      Get /api/users/profile
+// @access     Private
+
+const getUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if(user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    } else {
+        res.status(404)
+        throw new Error('User is not found')
+    }
+
+    res.send('success')
+})
+
+export { authUser, getUserProfile }
