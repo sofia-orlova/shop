5d35b2bfaf60ece71de4b23cb80acc16b913a32f
Sofia Orlova <sofia.orlova@telekom.com>
2021-10-01 09:38:21 +0300

    checkout_process_2: get order by id endpoint + order details reducer and action

--- backend/controlers/orderController.js
+++ backend/controlers/orderController.js
@@ -3,7 +3,7 @@ import OrderModel from "../models/orderModel.js";
 import Order from "../models/orderModel.js";
 
 // @desc       Create new order
-// @route      POST /api/order
+// @route      POST /api/orders
 // @access     Private
 
 const addOrderItems = asyncHandler(async (req, res) => {
@@ -40,5 +40,19 @@ const addOrderItems = asyncHandler(async (req, res) => {
 })
 
 
+// @desc       Get order by ID
+// @route      GET /api/orders/:id
+// @access     Private
 
-export { addOrderItems }
+const getOrderById = asyncHandler(async (req, res) => {
+    const order= await Order.findById(req.params.id).populate('user', 'name email')
+
+    if(order) {
+        res.json(order)
+    } else {
+        res.status(404)
+        throw new Error('Order not found')
+    }
+})
+
+export { addOrderItems, getOrderById }
