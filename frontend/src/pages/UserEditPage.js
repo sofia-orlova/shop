11f0741c1602d1be001ae58b09adbb96db30de4a
Sofia Orlova <sofia.orlova@telekom.com>
2021-10-14 20:11:23 +0300

    admin_page_1: update user functionality

--- frontend/src/pages/UserEditPage.js
+++ frontend/src/pages/UserEditPage.js
@@ -7,9 +7,10 @@ import { useDispatch, useSelector } from 'react-redux';
 import Message from '../componets/Message';
 import Loader from '../componets/Loader';
 import FormContainer from '../componets/FormContainer';
-import { getUserDetails } from '../redux/actions/userActions';
+import { getUserDetails, updateUser } from '../redux/actions/userActions';
+import { USER_UPDATE_RESET } from '../constants/userConstants';
 
-const UserEditPage = ({ match }) => {
+const UserEditPage = ({ match, history }) => {
   const userId = match.params.id;
 
   const [name, setName] = useState('');
@@ -17,21 +18,31 @@ const UserEditPage = ({ match }) => {
   const [isAdmin, setIsAdmin] = useState(false);
 
   const dispatch = useDispatch();
+
   const userDetails = useSelector((state) => state.userDetails);
   const { loading, error, user } = userDetails;
 
+  const userUpdate = useSelector((state) => state.userUpdate);
+  const { loading: loadingUpdate, error: errorUpdate, success: successUpdate } = userUpdate;
+
   useEffect(() => {
-    if (!user.name || user._id !== userId) {
+    if (successUpdate) {
+      dispatch({ type: USER_UPDATE_RESET });
+      history.push('/admin/userlist');
+    } else if (!user.name || user._id !== userId) {
       dispatch(getUserDetails(userId));
     } else {
       setName(user.name);
       setEmail(user.email);
       setIsAdmin(user.isAdmin);
     }
-  }, [dispatch, userId, user]);
+  }, [dispatch, history, userId, user, successUpdate]);
 
   const submitHandler = (e) => {
     e.preventDefault();
+    dispatch(updateUser({
+      _id: userId, name, email, isAdmin,
+    }));
   };
 
   return (
@@ -39,6 +50,8 @@ const UserEditPage = ({ match }) => {
       <Link to="/admin/userlist" className="btn btn-light my-3">Go back</Link>
       <FormContainer>
         <h1>Edit User</h1>
+        {loadingUpdate && <Loader />}
+        {errorUpdate && <Message variant="danger">{errorUpdate}</Message> }
         {loading ? <Loader /> : error ? <Message variant="danger">{error}</Message> : (
           <Form onSubmit={submitHandler}>
             <Form.Group controlId="name">
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -4,7 +4,7 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import { productListReducer, productDetailsReducer } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
 import {
-  userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer, userListReducer, userDeleteReducer,
+  userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer, userListReducer, userDeleteReducer, userUpdateReducer,
 } from './reducers/userReducers';
 import {
   orderCreateReducer, orderDetailsReducer, orderPayReducer, orderListMyReducer,
@@ -20,6 +20,7 @@ const reducer = combineReducers({
   userUpdateProfile: userUpdateProfileReducer,
   userList: userListReducer,
   userDelete: userDeleteReducer,
+  userUpdate: userUpdateReducer,
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
