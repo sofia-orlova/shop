88d85895d050f00ccbf0dd3bf6d416db77e95916
Sofia Orlova <sofia.orlova@telekom.com>
2021-10-15 14:05:39 +0300

    admin_page_2: admin product list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import PlaceOrderPage from './pages/PlaceOrderPage';
 import OrderPage from './pages/OrderPage';
 import UserListPage from './pages/UserListPage';
 import UserEditPage from './pages/UserEditPage';
+import ProductListPage from './pages/ProductListPage';
 
 function App() {
   return (
@@ -33,6 +34,7 @@ function App() {
           <Route path="/placeorder" component={PlaceOrderPage} />
           <Route path="/order/:id" component={OrderPage} />
           <Route path="/admin/userlist" component={UserListPage} />
+          <Route path="/admin/productlist" component={ProductListPage} />
           <Route path="/admin/user/:id/edit" component={UserEditPage} />
         </Container>
       </main>
